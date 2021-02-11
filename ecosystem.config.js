b1410866b74640e85a892ce816335f7a1fb8d834
Long Nguyen <tamdaulong207@gmail.com>
2021-02-11 10:10:45 +0700

    config release

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    name: 'blackjack-web',
+    script: 'npm run release',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
--- src/common/connection.ts
+++ src/common/connection.ts
@@ -1,7 +1,7 @@
-//development
-export const BASE_API_URL = 'http://localhost:3009/api'
-export const SOCKET_URL = 'http://localhost:3009'
+// //development
+// export const BASE_API_URL = 'http://localhost:3009/api'
+// export const SOCKET_URL = 'http://localhost:3009'
 
 //production
-// export const BASE_API_URL = 'http://172.104.181.210:2409/api'
-// export const SOCKET_URL = 'http://172.104.181.210:2409'
+export const BASE_API_URL = 'http://172.104.181.210:4203/api'
+export const SOCKET_URL = 'http://172.104.181.210:4203'
